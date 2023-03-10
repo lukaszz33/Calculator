66d9aa5fa898ff549077a776b064c2c174f5aa27
Łukasz <user@MacBook-Air-User.local>
2023-03-10 12:12:36 +0100

    main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const currentNumber = document.getSelector ('.currentNumber');
+
+const previousNumber = document.querySelector ('.previousNumber p');
+
+const mathSingth = document.querySelector ('.mathSingth');
+
+const numberButtons = document.querySelectorAll ('.number');
+
+const operatorsButtons = document.querySelecetorAll('.operator');
+
+const equalsButton = document.querySelector ('.equals');
+
+const clearButton = document.querySelector ('.clear');
+
+const calculatorHistory = document.querySelector('.history');
+
+const historybtn = documentquerySelector('.history-btn');
+
