371e87681178a0d0de16bfb43169805ca77bb990
Łukasz <user@macbook-air-user.home>
2023-03-14 23:47:29 +0100

    main.js update

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const currentNumber = document.getSelector ('.currentNumber');
 
 const previousNumber = document.querySelector ('.previousNumber p');
 
-const mathSingth = document.querySelector ('.mathSingth');
+const mathSing = document.querySelector ('.mathSingth');
 
-const numberButtons = document.querySelectorAll ('.number');
+const numbersButtons = document.querySelectorAll ('.number');
 
 const operatorsButtons = document.querySelecetorAll('.operator');
 
@@ -15,4 +15,37 @@ const clearButton = document.querySelector ('.clear');
 const calculatorHistory = document.querySelector('.history');
 
 const historybtn = documentquerySelector('.history-btn');
+
+let result = '';
+
+function displayNumbers (){
+    if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
+    if(this.textContent === '.' && currentNumber.innerHTML ==='') return currentNumber.innerHTML = '.0'
+    currentNumber.innerHTML += this.textContent;
+}
+
+function operate (){
+    if(currentNumber.innerHTML === '' && this.textCintent === '-'){
+        currentNumber.innerHTML = '-';
+        return;
+    }
+}
+
+function showResult (){
+
+}
+
+function clearScreen (){
+
+}
+
+// BTN lisiner
+
+operatorsButtons.forEach((button) => button.addEventListener('click', operate))
+equalsButton.addEventListener('click', showResult);
+clearButton.addEventListener('click', clearScreen);
+numbersButtons.forEach((button) => {
+    button.addEventListener('click', displayNumbers);
+});
+historybtn.addEventListener('click', clearHistory);
 
